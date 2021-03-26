b6b48aadcd1c04c163359ad60a52e4c9e262a4a8
Kuryshev Nikolai <knk@ileasing.ru>
2021-03-26 22:50:23 +0300

    Рабочая конфигурация для сборки проекта с помощью docker-compose.

---
+++ server/src/main/java/ru/server/ServerApplication.java
@@ -0,0 +1,14 @@
+package ru.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
+@EnableAutoConfiguration
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+}
