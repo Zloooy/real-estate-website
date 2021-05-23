bbf988ea8473921f7db987fa19744c848c084877
Zloooy <gu1999yra@yandex.ru>
2021-05-24 02:31:23 +0300

    Доработка api коиплексов

--- server/src/main/java/ru/server/controllers/FlatController.java
+++ server/src/main/java/ru/server/controllers/FlatController.java
@@ -13,7 +13,6 @@ import java.util.List;
 import java.util.Optional;
 
 @CrossOrigin(origins = "*")
-
 @RestController
 @Api(value="Квартиры", description = "Поиск квартир", produces = "application/json")
 public class FlatController {
